4c7dcdf8a186885c2f589bd5799b9fadff0e6e37
agustinsilva447 <agustinsilva447@gmail.com>
2020-09-20 17:07:08 -0300

    unittest

--- Curso/C3C1.py
+++ Curso/C3C1.py
@@ -11,52 +11,117 @@ No es necesario hacer una interfaz gráfica (o de consola), sino que puede estar
 el funcionamiento validado con las pruebas unitarias.
 """
 import random
+import unittest
 
-articulos = {}
-descuentos = {}
-total_pesos = 0
-total_desc = 0
-caja = True
-
-while caja:
-    print("Agregue el código del artículo o ingrese la tecla f para finalizar:")
-    codigo = input()
-    if codigo == 'f':
-        caja = False
-    else:
+class CajaRegistradora(object):
+    def f_precios(self, codigo, articulos):
         if codigo in articulos.keys():
             precio = articulos[codigo]
         else:
             precio = random.randint(1, 200) / 2
             articulos[codigo] = precio
-
         print("El artículo",codigo,"vale ${:.2f}".format(precio))
+        return precio, articulos
+
+    def f_descuento(self, total_desc, descuentos, codigo, precio):
+        total_desc += descuentos[codigo] * precio
+        precio = (1 - descuentos[codigo]) * precio
+        print("Articulo con un descuento del %{:.2f}. Precio con descuento: ${:.2f}".format(100 * descuentos[codigo], precio))
+        return total_desc, precio
+
+    def f_descuentos(self, codigo, descuentos, total_desc, precio):
         if codigo in descuentos.keys():
-            total_desc += descuentos[codigo] * precio
-            precio = (1 - descuentos[codigo]) * precio
-            print("Articulo con un descuento del %{:.2f}. Valor actualizado: ${:.2f}".format(100 * descuentos[codigo], precio))
+            total_desc, precio = self.f_descuento(total_desc, descuentos, codigo, precio)
         else:
             if random.random()>0.7:
                 descuentos[codigo] = random.uniform(0.05, 0.4)
-                total_desc += descuentos[codigo] * precio
-                precio = (1 - descuentos[codigo]) * precio
-                print("Articulo con un descuento del %{:.2f}. Valor actualizado: ${:.2f}".format(100 * descuentos[codigo], precio))
-        
-        total_pesos += precio
-        print("Subtotal: ${:.2f}".format(total_pesos))
-        print(" ")
+                total_desc, precio = self.f_descuento(total_desc, descuentos, codigo, precio) 
+        return total_desc, precio, descuentos
 
-print(articulos)
-print("Total descuentos aplicados: ${:.2f}".format(total_desc))
+    def f_transaccion(self, flag, codigo, articulos, descuentos, total_desc, total_pesos):
+        if codigo == 'f':
+            flag = False
+            precio = 0
+        else:
+            precio, articulos = self.f_precios(codigo, articulos)
+            total_desc, precio, descuentos = self.f_descuentos(codigo, descuentos, total_desc, precio)
+            total_pesos += precio
+            print("Subtotal: ${:.2f} \n".format(total_pesos))
 
-adios = False
-while adios == False:
-    print("El precio total es: ${:.2f}. ¿Con cuánto desea abonar?".format(total_pesos))
-    cliente = float(input())
-    if cliente >= total_pesos:
-        vuelto = cliente - total_pesos
-        print(" ")
-        print("Su vuelto: ${:.2f}. Gracias vuelva pronto!".format(vuelto))
-        adios = True
-    else:
-        print("Dinero no suficiente")
+        return flag, articulos, total_desc, total_pesos
+
+    def f_pago(self, cliente, total_pesos):
+        if cliente >= total_pesos:
+            vuelto = cliente - total_pesos
+            print(" ")
+            print("Su vuelto: ${:.2f}. ¡Gracias vuelva pronto!".format(vuelto))
+            flag = True
+            return flag
+        else:
+            print("Dinero no suficiente.")
+            return False
+
+    articulos = {}
+    descuentos = {}
+    total_pesos = 0
+    total_desc = 0
+
+    def comenzar_caja(self):
+        flag = True
+        while flag:
+            print("Agregue el código del artículo o ingrese la tecla f para finalizar:")
+            codigo = input()
+            flag, articulos, total_desc, total_pesos = self.f_transaccion(flag, codigo, self.articulos, self.descuentos, self.total_desc, self.total_pesos)
+
+        print(articulos)
+        print("Total descuentos aplicados: ${:.2f}".format(total_desc))
+
+        flag = False
+        while not flag:
+            print("El precio total es: ${:.2f}. ¿Con cuánto desea abonar?".format(total_pesos))
+            cliente = float(input())
+            flag = self.f_pago(cliente, total_pesos)
+
+class CajaRegistradoraTest(unittest.TestCase):
+    def setUp(self):
+        self.caja = CajaRegistradora()
+    
+    articulos_test = {'codigo_test1': 25, 'codigo_test2': 50}
+    descuentos_test = {'codigo_test1': 0.3}
+    codigo_test = 'codigo_test1'  
+    total_desc_test = 0  
+    precio_test = 25
+    flag_test = True
+    total_pesos_test = 0
+    cliente_test = 50
+
+    def test_f_precios(self):
+        caja = self.caja.f_precios(self.codigo_test, self.articulos_test)
+        self.assertEqual((25, self.articulos_test), caja)    
+
+    def test_f_descuento(self):
+        caja = self.caja.f_descuento(self.total_desc_test, self.descuentos_test, self.codigo_test, self.precio_test)   
+        self.assertEqual((7.5, 17.5), caja)      
+
+    def test_f_descuentos(self):
+        caja = self.caja.f_descuentos(self.codigo_test, self.descuentos_test, self.total_desc_test, self.precio_test)
+        self.assertEqual((7.5, 17.5, self.descuentos_test), caja) 
+
+    def test_f_transaccion(self):
+        caja = self.caja.f_transaccion(self.flag_test, self.codigo_test, self.articulos_test, self.descuentos_test, self.total_desc_test, self.total_pesos_test)  
+        self.assertEqual((True, self.articulos_test, 7.5, 17.5), caja) 
+
+    def test_f_transaccion2(self):
+        caja = self.caja.f_transaccion(self.flag_test, 'f', self.articulos_test, self.descuentos_test, self.total_desc_test, self.total_pesos_test)  
+        self.assertEqual((False, self.articulos_test, 0, 0), caja)        
+
+    def test_f_pago(self):
+        caja = self.caja.f_pago(self.cliente_test, 17.5)
+        self.assertEquals(True, caja)     
+
+    def test_f_pago2(self):
+        caja = self.caja.f_pago(self.cliente_test, 60)
+        self.assertEquals(False, caja)    
+
+if __name__ == '__main__':
+    unittest.main()        
--- Curso/auxiliar.py
+++ Curso/auxiliar.py
@@ -1,2 +1,5 @@
-import random
-print("Descuento del %{:.0f}".format(100 * random.uniform(0.05, 0.4)))
+def f_descuento(self, total_desc, descuentos, codigo, precio):
+    total_desc += descuentos[codigo] * precio
+    precio = (1 - descuentos[codigo]) * precio
+    print("Articulo con un descuento del %{:.2f}. Precio con descuento: ${:.2f}".format(100 * descuentos[codigo], precio))
+    return total_desc, precio
